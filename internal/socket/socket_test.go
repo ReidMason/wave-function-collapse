73d44e1fde8513af4ea2c0a4ceffd3529a913ee3
Reid Mason <maddogshain132@gmail.com>
2023-11-04 21:58:47 +0000

    Fixed naming for socket method

--- internal/socket/socket_test.go
+++ internal/socket/socket_test.go
@@ -18,7 +18,7 @@ func TestCanConnect(t *testing.T) {
 	for _, test := range tests {
 
 		r1 := CanConnect(Water, Sand)
-		r2 := CanConnectOld(Water, Sand)
+		r2 := canConnectHashmap(Water, Sand)
 		if r1 != r2 {
 			t.Fatalf("Failed comparison for %s and %s", fmt.Sprint(test[0]), fmt.Sprint(test[1]))
 		}
